ba05c1f6b07066a10cc3ce3efdcce9098b29aa08
Volodymyr <79228320+Fenix085@users.noreply.github.com>
2026-02-09 12:55:39 +0100

    feat: Enhance Twitter API interaction (probably, the account has been suspended, so, idk)
    Tweets flow like river streams
    Images now dance with text
    Code weaves new patterns

--- twitter.py
+++ twitter.py
@@ -1,7 +1,16 @@
 from common import *
 import tweepy
+import requests
+import io
 
-api = tweepy.Client(
+api11 = tweepy.API(tweepy.OAuth1UserHandler(
+    consumer_key=os.getenv("TWITTER_CONSUMER_KEY"), 
+    consumer_secret=os.getenv("TWITTER_CONSUMER_SECRET"), 
+    access_token=os.getenv("TWITTER_ACCESS_TOKEN"), 
+    access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+))
+
+api2 = tweepy.Client(
     bearer_token=os.getenv("TWITTER_BEARER_TOKEN"),
     consumer_key = os.getenv("TWITTER_CONSUMER_KEY"), 
     consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET"), 
@@ -10,6 +19,10 @@ api = tweepy.Client(
     )
 
 tweets_file = load_json('articles_tweets.json')
+EXTENTIONS = {
+    "image/jpeg": ".jpg",
+    "image/png": ".png"
+}
 
 def tweet_thread(tweets):
     for tweet in tweets:
@@ -18,12 +31,31 @@ def tweet_thread(tweets):
         tweet_id = None
         for t in thread:
             if tweet_id is None:
-                response = api.create_tweet(text=t)
+                media_id = upload_image(image) if image else None
+                media_ids = [media_id] if media_id else None
+                response = api2.create_tweet(text=t, media_ids=media_ids)
             else:
-                response = api.create_tweet(text=t, in_reply_to_tweet_id=tweet_id)
+                response = api2.create_tweet(text=t, in_reply_to_tweet_id=tweet_id)
             tweet_id = response.data['id']
 
+def get_image_url(image):
+    response = requests.get(image)
+    if response.status_code == 200:
+        return image
+    return None
+
+def upload_image(image_url):
+    image_data = requests.get(image_url).content
+    file_obj = io.BytesIO(image_data)
+    file_type = requests.head(image_url).headers['Content-Type']
+    ext = EXTENTIONS[file_type]
+    media = api11.media_upload(filename=f'temp{ext}', file=file_obj)
+    return media.media_id_string
+
 if __name__ == "__main__":
-    tweets_file = tweets_file[:1]
-    # print(tweets_file)
-    tweet_thread(tweets_file)
+    # tweets_file = tweets_file[:1]
+    # # print(tweets_file)
+    # tweet_thread(tweets_file)
+    # print(os.getenv("TWITTER_CONSUMER_KEY") is not None)
+    # print(os.getenv("TWITTER_ACCESS_TOKEN") is not None)
+    print(api11.verify_credentials())
